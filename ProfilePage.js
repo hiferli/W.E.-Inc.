b0b3395a1efb1bf6547372a94a1d7d0cf9cecbb3
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-03 03:17:34 +0530

    PFP Bug @ Edit Info Page Fix

--- ProfilePage.js
+++ ProfilePage.js
@@ -1,7 +1,7 @@
 const addUserDetails = () => {
     const name = JSON.parse(localStorage.getItem('userData')).name;
     const email = JSON.parse(localStorage.getItem('userData')).email;
-    const profilePicture = JSON.parse(localStorage.getItem('userData')).photoURL;
+    const profilePicture = JSON.parse(localStorage.getItem('userData')).profilePicture;
 
     const personalInformation = ` <div class="flex-grow-1 ms-3"><h2>${name}</h2><h3>${email}</h3></div>`
     const profilePictureTag = `<div class="flex-shrink-0"><img src=${profilePicture} alt = '${name}'/></div>`
